423aebbe26d1e958d954c85da1d6b3729eb31d15
SutMa <odineyez@gmail.com>
2023-07-31 17:32:39 -0600

    Fixed the get request on front and backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import mysql2 from "mysql2"
+import cors from "cors"
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 const db = mysql2.createConnection({
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
-function Books() {
+
+
+
+const Books = ()=> {
+  const [books,setBooks] = useState([])
+  useEffect(()=>{
+    const fetchAllBooks = async() =>{
+      try{
+        const res = await axios.get("http://localhost:8800/books")
+        setBooks(res.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fetchAllBooks()
+  },[])
+
   return (
-    <div>Books</div>
+    <div>
+      <h1>Book Shop</h1>
+      <div className="books">
+        {books.map(books=>{
+          <div className="book">
+            {<img src="" alt="" />}
+          </div> 
+        })}
+      </div>
+    </div>
   )
 }
 
 export default Books
+
